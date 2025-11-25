2b098620ef5df239f76cfe4b9a449117999140a6
haikeyi <4voacdo@gmail.com>
2025-11-25 11:09:30 +0800

    Modified test_calculator.py to give wrong values to test workflow failure

--- calculator.py
+++ calculator.py
@@ -10,10 +10,10 @@ def subtract(a, b):
 
 def multiply(a, b):
     """Return the product of two numbers."""
-    return a * b
+    return a ** b
 
 def divide(a, b):
     """Return the quotient of two numbers."""
     if b == 0:
         raise ValueError("Cannot divide by zero")
-    return a / b
+    return a % b
